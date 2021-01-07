ca6b5dd7589da2f60a6bafbabb50e5010b78ac43
Pedro0012021 <77128480+Pedro0012021@users.noreply.github.com>
2021-01-07 19:36:40 -0300

    Create código3numeros.py

---
+++ código3numeros.py
@@ -0,0 +1,13 @@
+Esse programa vai selecionar entre 3 números#Programa para verificar 3 números em Python
+
+
+numero = input('Digite um número ')
+
+if numero == '1':
+  print('Você Ganhou!!!')
+elif numero == '3': 
+  print('Você Ganhou!!!')
+elif numero == '5':
+  print('Você Ganhou!!!')  
+else:
+  print('Você Perdeu o Jogo...')  
